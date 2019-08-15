9ad116c66a3901e8856a320647a56dd33352f165
nofar <nofar.alfasi@s.afeka.ac.il>
2019-08-15 15:31:14 +0300

    Add files via upload

---
+++ catalog/language/en-gb/common/footer.php
@@ -0,0 +1,18 @@
+<?php
+// Text
+$_['text_information']  = 'Information';
+$_['text_service']      = 'Customer Service';
+$_['text_extra']        = 'Extras';
+$_['text_contact']      = 'Contact Us';
+$_['text_return']       = 'Returns';
+$_['text_sitemap']      = 'Site Map';
+$_['text_manufacturer'] = 'Stickers';
+$_['text_voucher']      = 'Gift Certificates';
+$_['text_affiliate']    = 'Affiliate';
+$_['text_special']      = 'Specials';
+$_['text_account']      = 'My Account';
+$_['text_order']        = 'Order History';
+$_['text_wishlist']     = 'Wish List';
+$_['text_newsletter']   = 'Newsletter';
+$_['text_powered']      = '<a href="#" onclick="window.open("https://www.sitelock.com/verify.php?site=stickerscenter.org","SiteLock","width=600,height=600,left=160,top=170");" ><img class="img-responsive" alt="SiteLock" title="SiteLock" src="//shield.sitelock.com/shield/stickerscenter.org" /></a>';
+//$_['text_powered']      = 'Powered By <a href="http://www.opencart.com">OpenCart</a><br /> %s &copy; %s';
