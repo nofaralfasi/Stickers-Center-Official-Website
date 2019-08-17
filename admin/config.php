4f52ae7e0bd27e7be87158c697b38f34ef3198b2
nofar <nofar.alfasi@s.afeka.ac.il>
2019-08-17 17:25:30 +0300

    Add files via upload

---
+++ admin/config.php
@@ -0,0 +1,36 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://stickerscenter.org/admin/');
+define('HTTP_CATALOG', 'http://stickerscenter.org/');
+
+// HTTPS
+define('HTTPS_SERVER', 'https://stickerscenter.org/admin/');
+define('HTTPS_CATALOG', 'https://stickerscenter.org/');
+
+// DIR
+define('DIR_APPLICATION', '/home2/nofar/public_html/admin/');
+define('DIR_SYSTEM', '/home2/nofar/public_html/system/');
+define('DIR_IMAGE', '/home2/nofar/public_html/image/');
+define('DIR_STORAGE', '/home2/nofar/storage/');
+define('DIR_CATALOG', '/home2/nofar/public_html/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'nofar');
+define('DB_PASSWORD', 'R6p1UJt@2EVBAuR');
+define('DB_DATABASE', 'nofar_scdb');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
