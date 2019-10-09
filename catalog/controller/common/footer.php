ba44ae8524398edb12396aa79d8ca120d6a4044b
nofar <nofar.alfasi@s.afeka.ac.il>
2019-10-09 14:36:31 +0300

    Add files via upload

--- catalog/controller/common/footer.php
+++ catalog/controller/common/footer.php
@@ -2,11 +2,8 @@
 class ControllerCommonFooter extends Controller {
 	public function index() {
 		$this->load->language('common/footer');
-
 		$this->load->model('catalog/information');
-
 		$data['informations'] = array();
-
 		foreach ($this->model_catalog_information->getInformations() as $result) {
 			if ($result['bottom']) {
 				$data['informations'][] = array(
@@ -15,7 +12,6 @@ class ControllerCommonFooter extends Controller {
 				);
 			}
 		}
-
 		$data['contact'] = $this->url->link('information/contact');
 		$data['return'] = $this->url->link('account/return/add', '', true);
 		$data['sitemap'] = $this->url->link('information/sitemap');
@@ -28,37 +24,29 @@ class ControllerCommonFooter extends Controller {
 		$data['order'] = $this->url->link('account/order', '', true);
 		$data['wishlist'] = $this->url->link('account/wishlist', '', true);
 		$data['newsletter'] = $this->url->link('account/newsletter', '', true);
-
         $data['powered'] = '';
-//		$data['powered'] = sprintf($this->language->get('text_powered'), $this->config->get('config_name'), date('Y', time()));
-
+		$data['secure_payments'] = $this->language->get('secure_payments');
 		// Whos Online
 		if ($this->config->get('config_customer_online')) {
 			$this->load->model('tool/online');
-
 			if (isset($this->request->server['REMOTE_ADDR'])) {
 				$ip = $this->request->server['REMOTE_ADDR'];
 			} else {
 				$ip = '';
 			}
-
 			if (isset($this->request->server['HTTP_HOST']) && isset($this->request->server['REQUEST_URI'])) {
 				$url = ($this->request->server['HTTPS'] ? 'https://' : 'http://') . $this->request->server['HTTP_HOST'] . $this->request->server['REQUEST_URI'];
 			} else {
 				$url = '';
 			}
-
 			if (isset($this->request->server['HTTP_REFERER'])) {
 				$referer = $this->request->server['HTTP_REFERER'];
 			} else {
 				$referer = '';
 			}
-
 			$this->model_tool_online->addOnline($ip, $this->customer->getId(), $url, $referer);
 		}
-
 		$data['scripts'] = $this->document->getScripts('footer');
-		
 		return $this->load->view('common/footer', $data);
 	}
 }
