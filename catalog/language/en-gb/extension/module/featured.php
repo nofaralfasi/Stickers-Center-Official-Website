84eb22ef875c2a16ed0e68ae7516a8477a0c1fd0
nofar <nofar.alfasi@s.afeka.ac.il>
2019-10-11 15:53:00 +0300

    Add files via upload

--- catalog/language/en-gb/extension/module/featured.php
+++ catalog/language/en-gb/extension/module/featured.php
@@ -3,4 +3,18 @@
 $_['heading_title'] = 'Featured';
 
 // Text
+$_['text_living_room'] = 'Living room';
+$_['text_nursery'] = 'Nursery';
+$_['text_kitchen'] = 'Kitchen';
+$_['text_bedroom'] = 'Bedroom';
+$_['text_bathroom'] = 'Bathroon';
+$_['text_children'] = 'Children';
+$_['text_teens'] = 'Teens';
+$_['text_dining_room'] = 'Dining room';
+$_['text_glowing'] = 'Glowing';
+$_['text_butterflies'] = 'Butterflies';
+$_['text_flowers'] = 'Flowers';
+$_['text_3d'] = '3D';
+// $_['text_'] = '';
 $_['text_tax']      = 'Ex Tax:';
+$_['text_more']      = 'More >';
