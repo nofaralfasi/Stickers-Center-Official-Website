240f6a126aca86b9b4cac1880526aaed4c5a21b2
nofar <nofar.alfasi@s.afeka.ac.il>
2019-10-11 16:49:32 +0300

    Add files via upload

---
+++ admin/language/en-gb/common/footer.php
@@ -0,0 +1,5 @@
+<?php
+// Text
+//$_['text_footer']  = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' All Rights Reserved.';
+$_['text_footer']  = '';
+$_['text_version'] = 'Version %s';
---
+++ admin/language/en-gb/extension/module/d_security_module.php
@@ -0,0 +1,49 @@
+<?php
+/*
+ *  location:                     admin/language
+ */
+
+// HEADING
+$_['heading_title']         = '<span style="color:#449DD0; font-weight:bold"><i class="fa fa-diamond" aria-hidden="true"></i>  Sapphire - security module</span><span style="font-size:12px; color:#999"> by <a href="https://www.opencart.com/index.php?route=marketplace/extension&filter_member=Dreamvention%20O%C3%9C" style="font-size:1em; color:#999" target="_blank">Dreamvention</a></span>';
+$_['heading_title_main']    = 'Sapphire - security module';
+$_['heading_title_main_pro']    = 'Sapphire PRO - security module';
+
+$_['text_edit_security_module'] = 'Edit Sapphire - security module';
+$_['text_get_pro_version']      = 'Get PRO version and enjoy the full security coverage';
+
+$_['text_home']             = 'Home';
+$_['text_module']           = 'Modules';
+$_['text_dashboard']        = 'Dashboard';
+$_['text_scan']             = 'Scan';
+$_['text_firewall']         = 'Firewall';
+$_['text_statistic']        = 'Statistic';
+$_['text_account']          = 'Account';
+$_['text_settings']         = 'Setting';
+$_['text_help_me']          = 'Help me';
+$_['text_pro']              = 'PRO';
+$_['text_coming_soon']      = 'Soon';
+
+
+// BUTTON
+$_['button_save_and_stay']  = 'Save and Stay';
+$_['button_save']           = 'Save';
+$_['button_cancel']         = 'Cancel';
+$_['button_update']         = 'Check Update';
+$_['button_create_ticket']  = 'Create Ticket';
+
+// ERROR
+$_['error_permission']      = "You don't have permission to modify Sapphire - security module";
+
+// HELP ME
+$_['text_help_me_description']      = 'If your OpenCart Store is infected and you need immediate help, our highly qualified security specialists are here to assist you. To ask for a security checkup, please click the button bellow.';
+$_['help_help_me']                  = "* Please create a ticket and provide your Admin and FTP access data. Our support specialists will asses your shop's current security state and provide you with a solution. The checkup fee is $30. Implementation is your personal responsibility. If you would like our specialists to implement them, an extra fee may be applied.";
+
+// LOW PHP VERSION
+$_['text_low_php_welcome']          = 'Welcome to the Sapphire';
+$_['text_low_php_description']      = "We detect that you are using old PHP version <b>%s</b>, it's very <a class='unprotected' href='http://php.net/supported-versions.php' target='_blank'>unsecure</a>. Please contact your hosting provider and update your PHP instance to at least <b class='protected'>PHP 5.6</b>.";
+$_['text_low_php_help']             = "* After updating, come back here, and we will give you a more detailed report of your vulnerabilities, and also help eliminate them.";
+
+// ONLY PRO
+$_['text_only_pro']                 = 'Section locked';
+$_['text_only_pro_description']     = "This section of the module enabled for <b>Sapphire PRO</b> users only. Please buy the full version, and enjoy full security coverage.";
+$_['button_buy_pro']                = 'Buy Sapphire PRO';
